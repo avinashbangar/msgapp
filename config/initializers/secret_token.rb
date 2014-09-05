f15731b9340186bfc8dc695bcdee28407789a2fc
Avinash Bangar <abangar@proofpoint.com>
2014-09-05 10:06:01 -0700

    Commiting basic rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Msgapp::Application.config.secret_token = '1c0b4982aea7b8f2a64372aac8156e35e71b70108226f619c3aa8853a1340b3aeb55171a3832e8bd46ece50c68bb1630bcb0f95d9828224096196e16938eee18'
